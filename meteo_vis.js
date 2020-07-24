8eccf845b7c3fefb285e019271a3d58568ad8726
adfr96 <alexdellorco@gmail.com>
2020-07-24 22:41:55 +0200

    add dati_meteo.json

--- meteo_vis.js
+++ meteo_vis.js
@@ -1,8 +1,11 @@
 //var url = '../geojson-italy/topojson/limits_IT_provinces.topo.json'
 var url = "https://raw.githubusercontent.com/openpolis/geojson-italy/master/topojson/limits_IT_all.topo.json"
+
+
+
 d3.json(url, function(error, topology) {
-  if (error) throw error;
-  console.log("topojson", topology)
+    if (error) throw error;
+    console.log("topojson", topology)
     var width = 1200,
     height = 800;
 
@@ -11,34 +14,34 @@ d3.json(url, function(error, topology) {
     .attr("height", height);
 
     var projection = d3.geo.albers()
-      .center([-12, 41])
-      .rotate([-15, 0])
-      .parallels([20, 40])
-      .scale(3000)
-      .translate([0, height/2]);
+    .center([-12, 41])
+    .rotate([-15, 0])
+    .parallels([20, 40])
+    .scale(3000)
+    .translate([0, height/2]);
 
     //.translate([width/8, height*7/2])
     var path = d3.geo.path().projection(projection);
-    
-    
+
+
     var provinces = topojson.feature(topology, topology.objects.provinces);
     var municipalities = topojson.feature(topology,topology.objects.municipalities)
-    
+
     console.log("provinces", provinces)
     console.log("municipalities", municipalities)
-    
+
     svg.append("g")
-        .attr("class","italy")
-        .selectAll("path")
-        .data(provinces.features)
-        .enter()
-        .append("path")
-        .attr("d",path)
-        .attr("class",function(d){return d.properties.prov_acr});
-    
+    .attr("class","italy")
+    .selectAll("path")
+    .data(provinces.features)
+    .enter()
+    .append("path")
+    .attr("d",path)
+    .attr("class",function(d){return d.properties.prov_acr});
+
     torino = svg.selectAll(".TO").attr("fill","yellow");
     console.log(torino);
-    
+
     //italy.attr("transform", "translate(-500,2600)");
     
 });
